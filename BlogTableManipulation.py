b33e7643556cb84c812c018e6668e19c1ca1ecd1
Oğulcan Demirdizen <59333212+Ogi-Z@users.noreply.github.com>
2024-04-17 19:43:14 +0300

    Blog Add, Delete and Query functions updated

--- BlogTableManipulation.py
+++ BlogTableManipulation.py
@@ -2,27 +2,27 @@ import DbConnection as Db
 
 
 
-# User tablosuna veri ekleme fonksiyonu
-def add_user(user_id, username, usersurname, useremail, userpassword, usercity, role_id):
+# Blog tablosuna veri ekleme fonksiyonu
+def add_blog(user_id, blog_id, blog_category, blog_text):
     conn = Db.connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO User (UserID, UserName, UserSurname, UserEmail, UserPassword, UserCity, RoleID) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, username, usersurname, useremail, userpassword, usercity, role_id))
+    cursor.execute("INSERT INTO Blog (UserID, BlogID, BlogCategory, BlogText) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, blog_id, blog_category, blog_text))
     conn.commit()
     conn.close()
 
 # User tablosundan belirli bir UserID'ye göre veri silme fonksiyonu
-def delete_user(user_id):
+def delete_blog(blog_id):
     conn = Db.connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM User WHERE UserID=?", (user_id,))
+    cursor.execute("DELETE FROM Blog WHERE BlogID=?", (blog_id,))
     conn.commit()
     conn.close()
 
 # Belirli bir UserID'ye göre User tablosundan veri sorgulama fonksiyonu
-def query_user(user_id):
+def query_user(blog_id):
     conn = Db.connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM User WHERE UserID=?", (user_id,))
+    cursor.execute("SELECT * FROM Blog WHERE UserID=?", (blog_id,))
     user_data = cursor.fetchone()
     conn.close()
     return user_data
