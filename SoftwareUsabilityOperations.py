df4e5f95ff9231f237ef42dc8a41750c27c28cfc
Oğulcan Demirdizen <59333212+Ogi-Z@users.noreply.github.com>
2024-04-17 20:14:48 +0300

    SoftwareUsability Operations Change

--- SoftwareUsabilityOperations.py
+++ SoftwareUsabilityOperations.py
@@ -6,23 +6,23 @@ import DbConnection as Db
 def add_softwareUsability(user_id, SoftwareUsabilityID, SoftwareUsabilitySoftware, SoftwareUsabilityTopicName, SoftwareUsabilityText):
     conn = Db.connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO Blog (UserID, BlogID, BlogCategory, BlogText) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, SoftwareUsabilityID, SoftwareUsabilitySoftware, SoftwareUsabilityTopicName, SoftwareUsabilityText))
+    cursor.execute("INSERT INTO Blog (UserID, SoftwareUsabilityID, SoftwareUsabilitySoftware, SoftwareUsabilityTopicName, SoftwareUsabilityText) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, SoftwareUsabilityID, SoftwareUsabilitySoftware, SoftwareUsabilityTopicName, SoftwareUsabilityText))
     conn.commit()
     conn.close()
 
 # User tablosundan belirli bir BlogID'ye göre veri silme fonksiyonu
-def delete_softwareUsability(blog_id):
+def delete_softwareUsability():
     conn = Db.connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM Blog WHERE BlogID=?", (blog_id,))
+    cursor.execute("DELETE FROM Blog WHERE SoftwareUsabilityID=?", (SoftwareUsabilityID))
     conn.commit()
     conn.close()
 
 # Belirli bir BlogID'ye göre User tablosundan veri sorgulama fonksiyonu
-def query_softwareUsability(blog_id):
+def query_softwareUsability():
     conn = Db.connect_to_database()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Blog WHERE UserID=?", (blog_id,))
+    cursor.execute("SELECT * FROM SoftwareUsability")
     softwareUsability_data = cursor.fetchone()
     conn.close()
     return softwareUsability_data
