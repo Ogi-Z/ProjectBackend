b3650b03496f110a080221091dae56c84cced4a6
Oğulcan Demirdizen <59333212+Ogi-Z@users.noreply.github.com>
2024-04-17 19:17:09 +0300

    Add project files.

---
+++ UserTableManipulation.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+# Veritabanı dosyasının adı
+db_file = "tempDB"
+
+# Veritabanına bağlanma fonksiyonu
+def connect_to_database():
+    conn = sqlite3.connect(db_file)
+    return conn
+
+# User tablosuna veri ekleme fonksiyonu
+def add_user(user_id, username, usersurname, useremail, userpassword, usercity, role_id):
+    conn = connect_to_database()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO User (UserID, UserName, UserSurname, UserEmail, UserPassword, UserCity, RoleID) VALUES (?, ?, ?, ?, ?, ?, ?)", (user_id, username, usersurname, useremail, userpassword, usercity, role_id))
+    conn.commit()
+    conn.close()
+
+# User tablosundan belirli bir UserID'ye göre veri silme fonksiyonu
+def delete_user(user_id):
+    conn = connect_to_database()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM User WHERE UserID=?", (user_id,))
+    conn.commit()
+    conn.close()
+
+# Belirli bir UserID'ye göre User tablosundan veri sorgulama fonksiyonu
+def query_user(user_id):
+    conn = connect_to_database()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM User WHERE UserID=?", (user_id,))
+    user_data = cursor.fetchone()
+    conn.close()
+    return user_data
+
